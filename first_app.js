eade74156c977f313027faf47e5015c3425ad9f5
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-05-11 23:02:35 +0530

    Node.Js-Start

---
+++ first_app.js
@@ -0,0 +1,4 @@
+const fs =require('fs');
+fs.writeFileSync("hello text","Hello i used extended functionality of Node.js to access my local storage and create a folder in it")
+
+console.log("Hello world!")
