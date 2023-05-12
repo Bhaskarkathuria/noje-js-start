8ac8076bc51ce7b6d5474f60c4012895044f7751
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-05-13 02:57:04 +0530

    redirecting to different URL response

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
 const http=require('http');
-const fs=require('fs')
+const fs=require('fs');
 
-const port=4000;
+const server=http.createServer((req,res)=>{
+    const url=req.url;
+    const method=req.method;
+    if(url==='/'){
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body');
+    res.write('</html>');
+    return res.end();
 
-const server=http.createServer(function(request,response)
-{   
-    //console.log(request.url,request.method,request.headers)
-
-    response.writeHead(200,{'Contenr-Type':'text/html'})
-    fs.readFile('index.html',function(error,data)
-    {
-        if(error){
-            response.writeHead(404)
-            response.write('ERROR:File not Found')
-        }else{
-            response.write(data)
-        }
-        response.end()
-    })
-
-   
-    
-})
-
-server.listen(port,function(error){
-    if(error){
-        console.log('Something went wrong',error)
-    }else{
-        console.log('server is listning on port',port)
     }
-})
+    if(url==='/message' && method==='POST'){
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode=302;
+        res.setHeader('Location','/')
+        return res.end();
+    }
+    if(url==='/home'){
+       res.setHeader('Content-Type','text/html')
+    res.write('<html>');
+    res.write('<head><title>MY First Page</title><head>');
+    res.write('<body><h1> Welcome Home</h1></body');
+    res.write('</html>');
+    res.end();
+ 
+
+    }
+    if(url==='/about'){
+        res.setHeader('Content-Type','text/html')
+    res.write('<html>');
+    res.write('<head><title>MY First Page</title><head>');
+    res.write('<body><h1> Welcome to about us page</h1></body');
+    res.write('</html>');
+    res.end()
+    }
+    if(url==='/node'){
+                res.setHeader('Content-Type','text/html')
+    res.write('<html>');
+    res.write('<head><title>MY First Page</title><head>');
+    res.write('<body><h1> Welcome to my node Js project</h1></body');
+    res.write('</html>');
+    res.end()
+    }
+
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>');
+    res.write('<head><title>MY First Page</title><head>');
+    res.write('<body><h1> Hello from Node js Server!</h1></body');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(4000)
