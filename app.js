cb54c41ec937328f289c6738316c684a79e2cd4b
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-05-13 01:27:12 +0530

    Sending HTML file from server response

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const http=require('http');
+const fs=require('fs')
 
 const port=4000;
 
 const server=http.createServer(function(request,response)
-{
-    response.write("Bhaska Kathuria made this server")
-    response.end()
+{   
+    //console.log(request.url,request.method,request.headers)
+
+    response.writeHead(200,{'Contenr-Type':'text/html'})
+    fs.readFile('index.html',function(error,data)
+    {
+        if(error){
+            response.writeHead(404)
+            response.write('ERROR:File not Found')
+        }else{
+            response.write(data)
+        }
+        response.end()
+    })
+
+   
+    
 })
 
 server.listen(port,function(error){
