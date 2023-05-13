a751bf1ef64d8b5e4181b2d13ce587d964c81286
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-05-14 02:50:11 +0530

    clean UP code

--- app.js
+++ app.js
@@ -1,75 +1,76 @@
 const http=require('http');
-const fs=require('fs');
+//const fs=require('fs');
+const routs= require('./routes')
 
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    if(url==='/'){
+const server=http.createServer(routs)
+  //  const url=req.url;
+  //  const method=req.method;
+    // if(url==='/'){
 
-    fs.readFile('message.txt',(err,data)=>{
-        if(err){
-            console.log(err)
-        }
-        console.log('data from file',data)
-        res.write('<html>')
-        res.write('<head><title>Enter Message </title></head>')
-        res.write(`<body>${data}</body>`)
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body');
-        res.write('</html>');
-        return res.end();
-    })    
+    // fs.readFile('message.txt',(err,data)=>{
+    //     if(err){
+    //         console.log(err)
+    //     }
+    //     console.log('data from file',data)
+    //     res.write('<html>')
+    //     res.write('<head><title>Enter Message </title></head>')
+    //     res.write(`<body>${data}</body>`)
+    //     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body');
+    //     res.write('</html>');
+    //     return res.end();
+    // })    
     
  
 
-    }
-    if(url==='/message' && method==='POST'){
-        const body=[]
-        req.on('data',(chunk)=>{
-            console.log(chunk)
-            body.push(chunk)
-        })
-        req.on('end',()=>{
-            const parseBody=Buffer.concat(body).toString()
-            console.log(parseBody)
-            const message=parseBody.split('=')[1]
+    // }
+    // if(url==='/message' && method==='POST'){
+    //     const body=[]
+    //     req.on('data',(chunk)=>{
+    //         console.log(chunk)
+    //         body.push(chunk)
+    //     })
+    //     req.on('end',()=>{
+    //         const parseBody=Buffer.concat(body).toString()
+    //         console.log(parseBody)
+    //         const message=parseBody.split('=')[1]
 
-            fs.writeFile('message.txt',message,err=>{
-            res.statusCode=302;
-            res.setHeader('Location','/')
-            return res.end();
-            });
+    //         fs.writeFile('message.txt',message,err=>{
+    //         res.statusCode=302;
+    //         res.setHeader('Location','/')
+    //         return res.end();
+    //         });
 
-        })
+    //     })
 
-        //fs.writeFileSync('message.txt','DUMMY');
+    //     //fs.writeFileSync('message.txt','DUMMY');
         
-    }
-    if(url==='/home'){
-       res.setHeader('Content-Type','text/html')
-    res.write('<html>');
-    res.write('<head><title>MY First Page</title><head>');
-    res.write('<body><h1> Welcome Home</h1></body');
-    res.write('</html>');
-    res.end();
+    // }
+    // if(url==='/home'){
+    //    res.setHeader('Content-Type','text/html')
+    // res.write('<html>');
+    // res.write('<head><title>MY First Page</title><head>');
+    // res.write('<body><h1> Welcome Home</h1></body');
+    // res.write('</html>');
+    // res.end();
  
 
-    }
-    if(url==='/about'){
-        res.setHeader('Content-Type','text/html')
-    res.write('<html>');
-    res.write('<head><title>MY First Page</title><head>');
-    res.write('<body><h1> Welcome to about us page</h1></body');
-    res.write('</html>');
-    res.end()
-    }
-    if(url==='/node'){
-                res.setHeader('Content-Type','text/html')
-    res.write('<html>');
-    res.write('<head><title>MY First Page</title><head>');
-    res.write('<body><h1> Welcome to my node Js project</h1></body');
-    res.write('</html>');
-    res.end()
-    }
+    // }
+    // if(url==='/about'){
+    //     res.setHeader('Content-Type','text/html')
+    // res.write('<html>');
+    // res.write('<head><title>MY First Page</title><head>');
+    // res.write('<body><h1> Welcome to about us page</h1></body');
+    // res.write('</html>');
+    // res.end()
+    // }
+    // if(url==='/node'){
+    //             res.setHeader('Content-Type','text/html')
+    // res.write('<html>');
+    // res.write('<head><title>MY First Page</title><head>');
+    // res.write('<body><h1> Welcome to my node Js project</h1></body');
+    // res.write('</html>');
+    // res.end()
+    // }
 
     // res.setHeader('Content-Type','text/html')
     // res.write('<html>');
@@ -77,6 +78,6 @@ const server=http.createServer((req,res)=>{
     // res.write('<body><h1> Hello from Node js Server!</h1></body');
     // res.write('</html>');
     // res.end();
-});
+
 
 server.listen(4000)
