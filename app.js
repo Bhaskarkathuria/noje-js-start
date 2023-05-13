221baf4e1088f1d3a36b384fec040e6db069c229
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-05-14 02:21:57 +0530

    I want you to read all the messages from the file and show it at the top of the form. Everytime you add a new message it should show at the top of the form.

--- app.js
+++ app.js
@@ -5,18 +5,44 @@ const server=http.createServer((req,res)=>{
     const url=req.url;
     const method=req.method;
     if(url==='/'){
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body');
-    res.write('</html>');
-    return res.end();
+
+    fs.readFile('message.txt',(err,data)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log('data from file',data)
+        res.write('<html>')
+        res.write('<head><title>Enter Message </title></head>')
+        res.write(`<body>${data}</body>`)
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body');
+        res.write('</html>');
+        return res.end();
+    })    
+    
+ 
 
     }
     if(url==='/message' && method==='POST'){
-        fs.writeFileSync('message.txt','DUMMY');
-        res.statusCode=302;
-        res.setHeader('Location','/')
-        return res.end();
+        const body=[]
+        req.on('data',(chunk)=>{
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end',()=>{
+            const parseBody=Buffer.concat(body).toString()
+            console.log(parseBody)
+            const message=parseBody.split('=')[1]
+
+            fs.writeFile('message.txt',message,err=>{
+            res.statusCode=302;
+            res.setHeader('Location','/')
+            return res.end();
+            });
+
+        })
+
+        //fs.writeFileSync('message.txt','DUMMY');
+        
     }
     if(url==='/home'){
        res.setHeader('Content-Type','text/html')
@@ -45,12 +71,12 @@ const server=http.createServer((req,res)=>{
     res.end()
     }
 
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>');
-    res.write('<head><title>MY First Page</title><head>');
-    res.write('<body><h1> Hello from Node js Server!</h1></body');
-    res.write('</html>');
-    res.end();
+    // res.setHeader('Content-Type','text/html')
+    // res.write('<html>');
+    // res.write('<head><title>MY First Page</title><head>');
+    // res.write('<body><h1> Hello from Node js Server!</h1></body');
+    // res.write('</html>');
+    // res.end();
 });
 
 server.listen(4000)
